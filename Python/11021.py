64689afec3f0fc4312dd007bc1521ed94d894d8f
KimJeongwook <wookiigg@gmail.com>
2023-08-10 23:19:00 +0900

    Delete Python directory

--- Python/11021.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-num = int(input())
-for i in range(num):
-    a, b = map(int, sys.stdin.readline().split())
-    print(f"Case #{i + 1}: {a + b}")
--- Python/2908.py
+++
@@ -1,7 +0,0 @@
-num = list(map(int, input().split(' ')))
-reverse_num = []
-
-for i in num:
-    reverse_num.append(int(''.join(list(reversed(list(str(i)))))))
-
-print(max(reverse_num))
